95964f3cc3e5e80871ae4fa3979dfaac1e31f11f
Bonnie Wang <bonnie.wang.423@gmail.com>
2023-10-22 13:54:42 +1300

    add upload local file option

--- server/routes.js
+++ server/routes.js
@@ -1,27 +1,47 @@
 import express from 'express'
 import * as lib from './lib.js'
+import multer from 'multer'
 
 const router = express.Router()
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({ storage: storage })
+
 router.get('/', async (req, res) => {
   const puppies = await lib.getPuppiesData()
- 
 
   res.render('home', puppies)
-  // res.send(`Hello`)
 })
 
 router.get('/new', (req, res) => {
   res.render('new')
 })
 
-router.post('/new',async (req,res)=>{
-  const {name, owner, image, breed} = req.body
-  const puppyData = await lib.getPuppiesData()
-  const id = puppyData.puppies.length + 1
-  const puppy = { id, name, owner, image,breed}
-  lib.addPuppy(puppy)
-  res.redirect(`/${id}`)
+router.post('/new', upload.single('image'), async (req, res) => {
+  try {
+    let image = ''
+    if (req.file) {
+      image = `images/${req.file.originalname}`
+    } else if (req.body.imageURL) {
+      image = req.body.imageURL
+    }
+    const { name, owner, breed } = req.body
+    const puppyData = await lib.getPuppiesData()
+    const id = puppyData.puppies.length + 1
+    const puppy = { id, name, owner, image, breed }
+    lib.addPuppy(puppy)
+    res.redirect(`/${id}`)
+  } catch (err) {
+    console.error(err)
+  }
 })
 
 router.get('/:id', async (req, res) => {
@@ -61,13 +81,13 @@ router.post('/edit/:id', async (req, res) => {
 
 router.post('/delete/:id', async (req, res) => {
   try {
-    const id = Number(req.params.id);
-    await lib.deletePuppy(id);
-    res.render(`delete`);
+    const id = Number(req.params.id)
+    await lib.deletePuppy(id)
+    res.render(`delete`)
   } catch (err) {
-    console.error(err);
-    res.status(500).send('Failed to delete the puppy.');
+    console.error(err)
+    res.status(500).send('Failed to delete the puppy.')
   }
-});
+})
 
 export default router
